af7ce8e528f67f22ff1f94939ef40d52eb710d2a
Mahmoud Kassem <mahmoud.ali.kassem@gmail.com>
2023-02-26 16:51:30 +0200

    my app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  elArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   title = 'myapp';
 }
